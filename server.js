8dcf0fd3d60b835fbeda58ebf6c493ca0bbf755e
trantung <trantung1095@gmail.com>
2023-02-27 22:30:30 +0700

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const favoriteFood = process.env.FAVORITE_FOOD;
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function main() {
+  while (true) {
+    console.log("Microservices rock!");
+    console.log(`My favorite food is ${favoriteFood}`);
+    await sleep(5000);
+  }
+}
+
+main();
